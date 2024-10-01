3a9f087beab90f93aff229a49ff73e609705d2c9
Ola <oyinkansola_o@yahoo.com>
2024-09-30 22:05:48 -0700

    wtver"

--- game.py
+++ game.py
@@ -10,13 +10,35 @@ pygame.init()
 # set mode needs at least one arg - width and height
 screen = pygame.display.set_mode((800, 400))
 
+#creating a new surface 
+# test_surface = pygame.Surface((100,200))
+
+
+#creating a font (fonttype, fontsize)
+test_font = pygame.font.Font(None, 50)
+
+
+# adding color to surface 
+# test_surface.fill('Red')
+
+new_surface = pygame.image.load('background.jpg')
+# render takes 3 bits of information text, quality of text, color
+text_surface = test_font.render('my game', False, "Green")
+
+snail_surface = pygame.image.load("snail.png")
 
 #update pygame windo titltle 
 pygame.display.set_caption("Runner")
 # create a clock object - helps with time and frame rate 
+
+
+
 clock  = pygame.time.Clock()
 
 # going to run forever
+
+#snail_xposition
+snail_position_x = 600
 
 while True:
     # draw all element and update all element
@@ -29,6 +51,18 @@ while True:
             pygame.quit()
             #stops the while loop
             exit()
+    #blit stands for block image transfer, we want to get the left,top position 
+    screen.blit(new_surface,(200,100))
+
+    screen.blit(text_surface,(300,50))
+    # -1 moves to the left +1 moves to the right
+    snail_position_x-= 4
+    if snail_position_x < -100: snail_position_x = 800
+
+    screen.blit(snail_surface,(snail_position_x,250))
+
+
+    
     #updates the display surface
     pygame.display.update()
     # the clock tells the while loop not to run more than 60 seconds 
